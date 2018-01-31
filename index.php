6859b38b0a08b2575373e509d2267154948e68db
Takumi.Sakamoto <t.sakamoto.ouen@gmail.com>
2018-02-01 01:05:10 +0900

    [add]read news feed

--- index.php
+++ index.php
@@ -4,8 +4,14 @@ $newsXml = new SimpleXMLElement(file_get_contents("http://feeds.cnn.co.jp/rss/cn
 
 $result = [];
 foreach ($newsXml->item as $item) {
-	$result[] = ['title' => $item->title, 'link' => $item->link, 'image' => $item->description];
 	$str = (string)$item->description;
 	preg_match("/<img src='(.*)'/", $str, $matches);
-	var_dump($matches);
+	$image = isset($matches[1]) ? $matches[1] : '';
+	$result[] = [
+		'title' => $item->title,
+		'link' => $item->link,
+		'image' => $image
+	];
 }
+
+echo json_encode($result);
