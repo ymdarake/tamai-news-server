f7b9ed01202eb740e35b22f12f2442e68e10590f
Takumi.Sakamoto <t.sakamoto.ouen@gmail.com>
2018-02-01 00:45:29 +0900

    [add]read news feed

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo "hello, world";
+$newsXml = new SimpleXMLElement(file_get_contents("http://feeds.cnn.co.jp/rss/cnn/cnn.rdf"));
+
+var_dump($newsXml);
+
+foreach ($newsXml->item as $item) {
+	echo $item->title;
+	echo $item->link;
+}
