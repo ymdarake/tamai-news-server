2ef00f32bf8831a2ce1c220cd32864d1179be7a5
Takumi.Sakamoto <t.sakamoto.ouen@gmail.com>
2018-02-01 22:41:22 +0900

    [fix]max length

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+defne("LINE_DESCRIPTION_MAX_LENGTH", 60);
+
 require_once(__DIR__ . '/vendor/autoload.php');
 
 $newsXml = new SimpleXMLElement(file_get_contents("http://feeds.cnn.co.jp/rss/cnn/cnn.rdf"));
@@ -11,7 +13,7 @@ foreach ($newsXml->item as $item) {
 	$image = isset($matches[1]) ? $matches[1] : '';
 	$result[] = [
 		'title' => (string)$item->title,
-		'description' => mb_substr(str_replace('<p>', '', str_replace('<![CDATA[<p>', '', $description)), 0, 80, 'UTF-8'),
+		'description' => mb_substr(str_replace('<p>', '', str_replace('<![CDATA[<p>', '', $description)), 0, LINE_DESCRIPTION_MAX_LENGTH, 'UTF-8'),
 		'link' => substr($item->link, 0, -strlen('?ref=rss')),
 		'image' => $image
 	];
